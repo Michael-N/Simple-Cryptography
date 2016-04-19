fec6ef368accc770368105550010e8bb7b76fd95
Michael-N <soccer72msn2@gmail.com>
2016-04-18 20:53:56 -0400

    worked on caesar

--- crypto.py
+++ crypto.py
@@ -1,14 +1,53 @@
-class caesarMsg(object):
-    def __init__(self,keys,msg):
-        self.keys = []
+class caesar(object):
+    def __init__(self):
         self.cipherType = 'caesar'
-        self.msg = msg
-    def encrypt(self):
+	def chrToNum(char):
+		#converts to lower case
+		num = ord(char)
+		num -= 97
+		return num
+	def numToChr(num):
+		#accepts numbers in lowercase ascii form
+		char = ord(num + 97)
+		return char
+    def encrypt(self,msg,key):
         #ENCRYPTION CODE
-    def decrypt(self):
+		encrypted = ''
+		for letter in msg:
+			letter = letter.lower()
+			if 97 <= ord(letter) <= 122:
+				encrypted += numToChr((chrToNum(letter) + key) % 26)
+			else:
+				continue
+		return encrypted
+		
+    def decrypt(self,msg,key):
         #DECRYPTION CODE
-    def frequencyAnalysis(self):
+		decrypted = ''
+		for letter in msg:
+			letter = letter.lower()
+			if 97 <= ord(letter) <= 122:
+				decrypted += numToChr((chrToNum(letter) - key) % 26)
+			else:
+				continue
+		return decrypted
+		
+    def frequencyAnalysis(self,msg):
         #ANALYSIS CODE
+		frequency = [0] * 26
+		for letter in msg:
+			if 97 <= ord(letter) <= 122:
+				frequency[chrToNum(letter)] += 1
+			else:
+				continue
+		total = 0
+		for index in range(0,26):
+			total += frequency[index]
+		total = float(total)
+		for indexAlpha in range(0,len(frequency)):
+			frequency[indexAlpha] = frequency[indexAlpha] / total
+		return frequency
+		
     def msgVariation(self):
         #MSG VARIATION CODE
     def keysValidate(self):
