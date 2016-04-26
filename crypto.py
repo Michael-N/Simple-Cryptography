2b5601ed64b3f598b8a5813892555d561880a0cb
Michael-N <soccer72msn2@gmail.com>
2016-04-25 21:44:52 -0400

    added in support for character maping for encrypting

--- crypto.py
+++ crypto.py
@@ -1,75 +1,132 @@
-def opp(t):
-	if t == True:
-		return False
-	elif t == False:
-		return True
-
+class cryptoErr(BaseException):
+	#This is the error class that will be raised if there is a problem
+	def __init__():
+		self.errName = ''
+		self.errValue = None
+		self.errHandler = None
+		self.errMetaData = ''
+		self.errUsrMsg = ''
+		return
+	def handle(self):
+		if self.errHandler != None:
+			try:
+				errHandler()
+			except:
+				return
+		else:
+			return
+class map(object):
+	def __init__(self,mapI,err = -1,):
+		#consider adding a map sequence so that certin letters map to certin numbers
+		#mapSequence = range(0,len(mapI))
+		self.mapLength = len(mapI)
+		self.mapI = mapI
+		self.err = err
+		self.mod = len(mapI)
+	def __call__(self,chrNum):
+		#will convert any string to a chr and any chr to a num according to the map
+		if type(chrNum) == str:
+			for index in range(0,self.mapLength):
+				if chrNum == self.mapI[index]:
+					return index
+				else:
+					continue
+			return self.err
+		elif type(chrNum) == int:
+			if self.mapLength <= chrNum <= self.mapLength:
+				return self.mapI[chrNum]
+			else:
+				return self.err
+		else:
+			raise cryptoErr
 class caesar(object):
 	def __init__(self):
 		self.cipherType = 'caesar'
+		self.defMap = map('abcdefghijklmnopqrstuvwxyz')
+		return
+	def opp(t):
+		if t == True:
+			return False
+		elif t == False:
+			return True
+	def chrToNum(self, char):
+		#converts a character to a number based upon the classes self.defMap
+		num = self.defMap(char)
+		return 	num
+	def setMap(self,mapN):
+		#takes a map as an argument
+		self.defMap = mapN
 		return 
-	def chrToNum(char):
-		#converts to lower case
-		num = ord(char)
-		num -= 97
-		return num	
-	def numToChr(num):
-		#accepts numbers in lowercase ascii form
-		char = ord(num + 97)
+	def numToChr(self, num):
+		#converts a number to a character based upon the classes self.defMap
+		char  = self.defMap(num)
 		return char
-	def az(letter):
+	def az(self, letter):
+		#checks if a letter is a to z 
 		if 97 <= ord(letter) <= 122:
 			return True
 		else:
-			return False  
+			return False
+	def sanitizeMsg(self,msg,rmvChrs):
+		for index in range(0, len(msg)):
+			for letterB in rmvChrs:
+				if msg[index] == letterB:
+					msg[index] == ''
+				else:
+					continue
+		return msg
 	def encrypt(self,msg,key):
 		#ENCRYPTION CODE
 		encrypted = ''
 		for letter in msg:
-			letter = letter.lower()
-			if self.az(letter):
-				encrypted += numToChr((chrToNum(letter) + key) % 26)
+			en = self.defMap(letter)
+			if en == self.defMap.err:
+				pass
 			else:
-				continue
+				encrypted += self.numToChr((self.chrToNum(letter) + key) % self.defMap.mod)
 		return encrypted
 	def decrypt(self,msg,key):
 		#DECRYPTION CODE
 		decrypted = ''
 		for letter in msg:
-			letter = letter.lower()
-			if az(letter):
-				decrypted += numToChr((chrToNum(letter) - key) % 26)
+			de = self.defMap(letter)
+			if de == self.defMap.err:
+				pass
 			else:
-				continue
+				decrypted += self.numToChr((self.chrToNum(letter) - key) % self.defMap.mod)
 		return decrypted
 	def frequencyAnalysis(self,msg):
 		#ANALYSIS CODE
-		frequency = [0] * 26
+		frequency = [0] * self.defMap.mod
 		for letter in msg:
-			if 97 <= ord(letter) <= 122:
-				frequency[chrToNum(letter)] += 1
+			fe = self.defMap(letter)
+			if fe == self.defMap.err:
+				pass
 			else:
-				continue
+				frequency[self.chrToNum(letter)] += 1
 		total = 0
-		for index in range(0,26):
+		for index in range(0,self.defMap.mod):
 			total += frequency[index]
 		total = float(total)
 		for indexAlpha in range(0,len(frequency)):
 			frequency[indexAlpha] = frequency[indexAlpha] / total
 		return frequency
 	def variance(self,frequency):
-		#VARIATION CODE
-		variance = 0
-		englishVariance = [0.0867,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.02361,0.00150,0.01974,0.00074]
-		for index in range(0,26):
-			variance += abs(englishVariance[index]-frequency[index])
-		return variance
+		#VARIATION CODE only works for msgs with a self.defMap = map('abcdefghijklmnopqrstuvwxyz')
+		if len(frequency) > 26:
+			raise BaseException
+		else:
+			variance = 0
+			englishVariance = [0.0867,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.02361,0.00150,0.01974,0.00074]
+			for index in range(0,26):
+				variance += abs(englishVariance[index]-frequency[index])
+			return variance
 	def noKeysDecrypt(self,msg):
 		#NO KEY DECRYPTION CODE # returns a list where the first item is the decrypted msg and the 2nd the key 
 		key = 0
 		lowestVariance = 1000
 		testKey = 0
-		for index in range(0,26):
+		for index in range(0,self.defMap.mod):
 			testDecrypt = self.decrypt(msg,key)
 			testFrequency = self.frequencyAnalysis(testDecrypt)
 			testVariance = variance(testFrequency)
@@ -87,7 +144,8 @@ class afine(caesar):
 		return num
 	def isRelPrime(n,a):
 		#checks if two numbers are relativly prime
-	def GFC(num,numA):
+		return True
+	def GFC(num, numA):
 		#Get the greatest common factor of the two numbers
 		return True
 	def getValidMultkeys(num):
@@ -148,7 +206,7 @@ class afine(caesar):
 			return False
 		elif True:
 			for letter in keys:
-				if opp(self.az(letter)):
+				if self.opp(self.az(letter)):
 					return False
 		else:
 			return True
